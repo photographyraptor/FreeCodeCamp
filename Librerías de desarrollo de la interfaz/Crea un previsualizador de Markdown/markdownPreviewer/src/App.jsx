ef2ca7f0f9cbc4f3304dd349660bdad85f3f18d5
photographyraptor <zdovahzaan@gmail.com>
2023-02-08 00:52:34 +0100

    markdown preview

--- Librerías de desarrollo de la interfaz/Crea un previsualizador de Markdown/markdownPreviewer/src/App.jsx
+++ Librerías de desarrollo de la interfaz/Crea un previsualizador de Markdown/markdownPreviewer/src/App.jsx
@@ -1,15 +1,65 @@
 import { useState } from 'react'
+import { marked } from 'marked';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [previewer, setPreview] = useState(0)
+  const handleChange = (event) => {
+    setPreview(marked(event.target.value));
+  };
+  const editorText = String.raw`# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  \/\/ this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)";`;
 
   return (
-    <div className="App">      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <div className="toolbar">Editor</div>
+      <div className="editor">
+        <textarea className="textarea" onChange={handleChange}>{editorText}</textarea>
+      </div>
+      <div className="toolbar">Previewer</div>
+      <div className="preview">      
+        <textarea className="textarea" value={previewer} readOnly></textarea>
       </div>
       <p className="doneBy">
         by PhotographyRaptor
