863fa1ff7d1a7276e8a1d980ed2bb6a0b9d42e20
photographyraptor <zdovahzaan@gmail.com>
2023-02-08 01:26:17 +0100

    electricbattery

--- Librerías de desarrollo de la interfaz/Crea una batería electrónica/electronicbattery/src/App.jsx
+++ Librerías de desarrollo de la interfaz/Crea una batería electrónica/electronicbattery/src/App.jsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function handleKeyPress(e) {
+  console.log(e.key);
+
+  if (e.key === 'a') {
+    var audio = new Audio('../assets/Heater-1.mp3');
+    audio.play();
+  } else if (e.key === 's') {
+    var audio = new Audio('../assets/Heater-2.mp3');
+    audio.play();
+  } else if (e.key === 'd') {
+    var audio = new Audio('../assets/Heater-3.mp3');
+    audio.play();
+  } else if (e.key === 'f') {
+    var audio = new Audio('../assets/Heater-4.mp3');
+    audio.play();
+  }
+}
+
+function App() {  
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   return (
     <div className="App">      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => {
+          var audio = new Audio('../assets/Heater-1.mp3');
+          audio.play();}}>A
+        </button>
+        <button onClick={() => {
+          var audio = new Audio('../assets/Heater-2.mp3');
+          audio.play();}}>S
+        </button>
+        <button onClick={() => {
+          var audio = new Audio('../assets/Heater-3.mp3');
+          audio.play();}}>D
+        </button>
+        <button onClick={() => {
+          var audio = new Audio('../assets/Heater-4.mp3');
+          audio.play();}}>F
         </button>
       </div>
       <p className="doneBy">
