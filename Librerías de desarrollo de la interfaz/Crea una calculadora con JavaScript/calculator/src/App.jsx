9486904f48b645bcdf31213be192273d693c5fbe
photographyraptor <zdovahzaan@gmail.com>
2023-02-08 12:26:11 +0100

    calc

--- Librerías de desarrollo de la interfaz/Crea una calculadora con JavaScript/calculator/src/App.jsx
+++ Librerías de desarrollo de la interfaz/Crea una calculadora con JavaScript/calculator/src/App.jsx
@@ -1,6 +1,40 @@
 import './App.css'
 import React from 'react'
 
+function calc(hist) {
+  let total = 0;
+  let sums = hist.split('+')
+  console.log("sums");
+  for(let sum in sums) {
+    let rests = sum.split('-')
+    let r = 0;
+    console.log("rests");
+    for(let rest in rests) {
+      let divs = rest.split('/')
+      let d = 1;
+      console.log("divs");
+      for(let div in divs) {
+        let mults = div.split('X')
+        let m = 1;
+        console.log("mults");
+        for(let mult in mults) {
+          mult = mult == 0 ? 1: mult;
+          m = m * mult;
+          console.log("m".concat(">").concat(m.toString()));
+        }
+        d = d / m;
+        console.log("d".concat(">").concat(d.toString()));
+      }
+      r = r - d;
+      console.log("r".concat(">").concat(r.toString()));
+    }
+    total = total + r;
+    console.log("s".concat(">").concat(total.toString()));
+  }
+  return total;
+}
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,35 +45,30 @@ class App extends React.Component {
   }
 
   handleClick = (v) => {
-    console.log(v);
+    console.log(v.toString());
     console.log(this.state);
     let hist = "";
     let val = "";
 
-    switch (v) { //pending setState
-      case "AC":
-        break;
-      case "/","X","-","+":
-        hist = this.state.historic + v;
-        break;
-      case "1","2","3","4","5","6","7","8","9","0",".":
-        hist = this.state.historic + v;
-        break;
-      case "=":
-        hist = this.state.historic + v;
-        val = "CALC"
-        hist = hist + ' ' + val;
-        break;
-      default:
-        break;
+    if (v == "AC") {
+    } else if (v == "/" || v == "X" || v == "-" || v == "+") {
+      hist = this.state.historic + v;
+    } else if (v == "1" || v == "2" || v == "3" 
+      || v == "4" || v == "5" || v == "6" || v == "."
+      || v == "7" || v == "8" || v == "9" || v == "0") {
+      hist = this.state.historic.concat(v.toString());
+    } else if (v == "=") {
+      //hist = this.state.historic + v;
+      val = calc(hist);
+      //hist = hist + ' = ' + val;
     }
-
-    console.log('hist -> ' + hist);
-    console.log('val -> ' + val)
+    
     this.setState({
       historic: hist,
       value: val
     });
+    console.log(this.state.historic);
+    console.log(this.state.val);
   };
 
   render () {
