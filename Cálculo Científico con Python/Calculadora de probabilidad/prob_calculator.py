46222622b73f4b3b6002abf353de989ef31cd4ef
photographyraptor <zdovahzaan@gmail.com>
2023-02-10 00:52:20 +0100

    calc prob

--- Cálculo Científico con Python/Calculadora de probabilidad/prob_calculator.py
+++ Cálculo Científico con Python/Calculadora de probabilidad/prob_calculator.py
@@ -2,7 +2,42 @@ import copy
 import random
 # Consider using the modules imported above.
 
+
 class Hat:
+    def __init__(self, **kwargs):
+        contents = []
+        for kwarg in kwargs:
+            for _i in range(0, kwargs[kwarg]):
+                contents.append(kwarg)
+        self.contents = contents
+
+    def draw(self, tries:int):
+        l = []
+        tries = min(tries, len(self.contents))
+        for _i in range(0, tries):
+            pos = random.randrange(len(self.contents))
+            l.append(self.contents.pop(pos))
+        return l
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat:Hat, expected_balls, num_balls_drawn, num_experiments):
+    matchedDrawn = 0
+    expectedBallsCount = 0
+    for type in expected_balls:
+        expectedBallsCount += expected_balls[type]
+
+    for _i in range(0, num_experiments):
+        newHat = copy.deepcopy(hat)
+        drawn = newHat.draw(num_balls_drawn)
+        expectedDraw = 0
+        for type in expected_balls:
+            for _j in range(0, expected_balls[type]):
+                try:
+                    drawn.remove(type)
+                except ValueError:                        
+                    continue
+                expectedDraw += 1
+        if (expectedDraw == expectedBallsCount):
+            matchedDrawn += 1 
+        
+    return matchedDrawn / num_experiments
