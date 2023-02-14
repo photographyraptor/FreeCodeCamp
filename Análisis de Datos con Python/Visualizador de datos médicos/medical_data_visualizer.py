3e299c3b3634a5ffae9b45375f8af0c67932e77f
photographyraptor <zdovahzaan@gmail.com>
2023-02-14 15:49:16 +0100

    catplot

--- Análisis de Datos con Python/Visualizador de datos médicos/medical_data_visualizer.py
+++ Análisis de Datos con Python/Visualizador de datos médicos/medical_data_visualizer.py
@@ -7,31 +7,33 @@ import numpy as np
 df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = df["weight"] / ((df["height"] / 100) ** 2)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df.loc[df["cholesterol"] <= 1, "cholesterol"] = 0
 df.loc[df["cholesterol"] > 1, "cholesterol"] = 1
 df.loc[df["gluc"] <= 1, "gluc"] = 0
 df.loc[df["gluc"] > 1, "gluc"] = 1
+df.loc[df["overweight"] > 25, "overweight"] = 1
+df.loc[df["overweight"] != 1, "overweight"] = 0
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = pd.melt(df, value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
-    print(df_cat)
-
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat = df.melt(value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
-
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat = df.melt(id_vars=["cardio"], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = pd.DataFrame(data=df_cat.value_counts(), columns=["total"]).sort_index().reset_index()
+    
     # Draw the catplot with 'sns.catplot()'
-
-
-
     # Get the figure for the output
-    fig = None
-
+    fig = sns.catplot(x="variable", 
+                y="total",
+                hue="value",
+                kind="bar",
+                col="cardio",
+                data=df_cat)
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
