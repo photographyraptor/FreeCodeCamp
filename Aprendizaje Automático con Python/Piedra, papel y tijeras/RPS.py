bfe076961576eed99e5f332972288c756bbd11e1
photographyraptor <zdovahzaan@gmail.com>
2023-02-23 22:38:45 +0100

    rps end

--- Aprendizaje Automático con Python/Piedra, papel y tijeras/RPS.py
+++ Aprendizaje Automático con Python/Piedra, papel y tijeras/RPS.py
@@ -1,10 +1,31 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+idealResponse = {'P': 'S', 'R': 'P', 'S': 'R'}
+options = ["R", "P", "S"]
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(prev_play, opponent_history: list=[]):
+    if prev_play != "":
+        opponent_history.append(prev_play)
+    else: # new oponent
+        opponent_history = []
 
-    return guess
+    if (len(opponent_history) < 4): # not enough history to make prediction
+        return idealResponse[options[random.randint(0,2)]]
+
+    # join last 4 plays by index
+    lastPlays = [ ''.join(opponent_history[k:k+4]) for k, v in enumerate(opponent_history[:-3]) ]
+    
+    # remove oldest and add possible next play    
+    nextPlays = [ ''.join([ *opponent_history[-3:], v ]) for v in options] 
+
+    # count repeated plays to find a pattern
+    sub_order = { k: lastPlays.count(k) for k in nextPlays } 
+    
+    # get most repeated play, then get the potential next play
+    prediction = max(sub_order, key=sub_order.get)[-1]
+    
+    return idealResponse[prediction]
+
+
+
