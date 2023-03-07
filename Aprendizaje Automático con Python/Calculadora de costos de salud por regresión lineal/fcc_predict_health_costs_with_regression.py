ff7895b82f3dd37f3a7d5b5240c8b57756a05997
photographyraptor <zdovahzaan@gmail.com>
2023-03-07 21:02:46 +0100

    predict health cost

--- Aprendizaje Automático con Python/Calculadora de costos de salud por regresión lineal/fcc_predict_health_costs_with_regression.py
+++ Aprendizaje Automático con Python/Calculadora de costos de salud por regresión lineal/fcc_predict_health_costs_with_regression.py
@@ -8,15 +8,61 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-import tensorflow_docs as tfdocs
-import tensorflow_docs.plots
-import tensorflow_docs.modeling
+# Print Callback f(x)
+class EpochDots(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 50 == 0:
+      print()
+      print('Epoch: {:d}, '.format(epoch), end='')
+      for name, value in sorted(logs.items()):
+        print('{}:{:0.4f}'.format(name, value), end=', ')
+      print()
+
+    print('.', end='')
 
 # Import data
 dataset = pd.read_csv('insurance.csv')
 dataset.tail()
 
+# Change categorical to numeric
+dataset['sex'].replace(['male', 'female'], [0, 1], inplace=True)
+dataset['smoker'].replace(['yes', 'no'], [1, 0], inplace=True)
+dataset['region'].replace(['southwest', 'southeast', 'northwest', 'northeast'], [1, 2, 3, 4], inplace=True)
+dataset.head()
 
+# Pop off expenses
+df_train = dataset.sample(frac=0.8, random_state=0)
+train_labels = df_train['expenses']
+train_dataset = df_train.drop('expenses', axis=1)
+
+df_test = dataset.drop(df_train.index)
+test_labels = df_test['expenses']
+test_dataset = df_test.drop('expenses', axis=1)
+
+# Build sequential model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(len(train_dataset.keys()),)),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
+
+model.compile(
+    optimizer= tf.keras.optimizers.RMSprop(0.05),
+    loss='mse',
+    metrics=['mae', 'mse']
+)
+
+model.summary()
+
+# Fit model
+r = model.fit(train_dataset, train_labels, epochs=500,
+              verbose=0, callbacks=[EpochDots()])
+
+# Evaluate model
+res = model.evaluate(test_dataset, test_labels, verbose=2)
+print(res) 
 
 # RUN THIS CELL TO TEST YOUR MODEL. DO NOT MODIFY CONTENTS.
 # Test model by checking how well the model generalizes using the test set.
